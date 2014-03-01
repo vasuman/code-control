a49448237cc50280f032c8982f1e3704a3ffad07
deepak1556 <hop2deep@gmail.com>
2014-03-02 04:32:27 +0530

    done with editor and aether integration

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ var ecstatic = require('ecstatic')(__dirname)
 var http = require('http')
 var URL = require('url')
 
-var bundle = require('./build')({
-    minify: false
-  , debug: true
-  , transforms: []
-})
-
 var rework = require('./rework')({
   debug: true
 })
@@ -15,7 +9,6 @@ var rework = require('./rework')({
 http.createServer(function(req, res) {
   var url = URL.parse(req.url).pathname
 
-  if (url === '/bundle.js') return liveBundle(req, res)
   if (url === '/style.css') return liveStyles(req, res)
 
   ecstatic(req, res, function(err) {
@@ -25,11 +18,6 @@ http.createServer(function(req, res) {
 }).listen(9966, function() {
   console.log('listening on http://localhost:9966')
 })
-
-function liveBundle(req, res) {
-  res.setHeader('Content-Type', 'application/javascript')
-  bundle().pipe(res)
-}
 
 function liveStyles(req, res) {
   res.setHeader('Content-Type', 'text/css')
--- src/index.js
+++ src/index.js
@@ -1,25 +1,121 @@
-// Modules
-//var aether = require('aether'); Y the F its not on npm??????? 
+(function() {
+  var Range, clearOutput, demoShowOutput, editors, aetherInput, grabDemoCode, lastAetherInput, lastJSInputAether, lastProblems, loadExample, oldConsoleLog, showProblems, watchForCodeChanges;
 
-// DOM Elements
-var dom = {
-  hud: document.getElementById('hud'),
-  editor: document.getElementById('javascript-input')
-}
+  editors = [];
 
-module.exports = Editor
+  Range = ace.require("ace/range").Range;
 
-function Editor() {
- var Range = ace.require('ace/range').Range, editor;
+  $(function() {
+    $(".ace-editor-wrapper").each(function() {
+      var editor;
+      editor = ace.edit(this);
+      editor.setTheme("ace/theme/xcode");
+      editor.getSession().setUseWorker(false);
+      editor.getSession().setMode("ace/mode/javascript");
+      editor.getSession().getDocument().on("change", watchForCodeChanges);
+      return editors.push(editor);
+    });
+    loadConfig();
+  });
 
- editor = ace.edit(dom.editor);
- editor.setTheme('ace/theme/xcode');
- editor.getSession().setUseWorker(false);
- editor.getSession().setMode('ace/mode/javascript');
- //editor.getSession().getDocument().on('change', watchForCodeChanges);
+  grabDemoCode = function() {
+    return editors[0].getValue() ;
+  };
 
- return editor;
-}
+  lastProblems = null;
 
-// Start
-new Editor
+  showProblems = function(aether) {
+    var allProblems, ann, annotations, editor, level, problem, problemIndex, problems, session, text, worst, wrapper, _ref;
+    if (_.isEqual(aether.problems, lastProblems)) {
+      return;
+    }
+    lastProblems = aether.problems;
+
+    for (level in aether.problems) {
+      problems = aether.problems[level];
+      for (problemIndex in problems) {
+        problem = problems[problemIndex];
+        if (problem.serialize) {
+          problems[problemIndex] = problem.serialize();
+        }
+      }
+    }
+
+    editor = editors[0];
+    session = editor.getSession();
+    annotations = [];
+    allProblems = aether.getAllProblems();
+    for (problemIndex in allProblems) {
+      problem = allProblems[problemIndex];
+      if (((_ref = problem.ranges) != null ? _ref[0] : void 0) == null) {
+        continue;
+      }
+      ann = {
+        row: problem.ranges[0][0].row,
+        column: problem.ranges[0][0].col,
+        raw: problem.message,
+        text: problem.message,
+        type: problem.level || "error"
+      };
+      annotations.push(ann);
+    }
+    session.setAnnotations(annotations);
+    wrapper = $("#worst-problem-wrapper").empty();
+    worst = allProblems[0];
+    if (worst) {
+      text = worst.type + " " + worst.level;
+      text += ": " + worst.message;
+      wrapper.text(text);
+      wrapper.toggleClass("error", worst.level === "error");
+      wrapper.toggleClass("warning", worst.level === "warning");
+      return wrapper.toggleClass("info", worst.level === "info");
+    }
+  };
+
+  demoShowOutput = function(aether) {
+    showProblems(aether);
+  };
+
+  clearOutput = function() {
+    $("#aether-console").text("");
+  };
+
+  lastJSInputAether = new Aether();
+
+  lastAetherInput = "";
+
+  watchForCodeChanges = function() {
+    var aetherInput, code;
+    aetherInput = editors[1].getValue();
+    code = grabDemoCode();
+    if (!Aether.hasChangedSignificantly(code, lastJSInputAether.raw) && aetherInput === lastAetherInput) {
+      return;
+    }
+    clearOutput();
+    lastAetherInput = aetherInput;
+    lastJSInputAether.transpile(code);
+    return eval(aetherInput);
+  };
+
+  watchForCodeChanges = _.debounce(watchForCodeChanges, 1000);
+
+  oldConsoleLog = console.log;
+
+  console.log = function() {
+    var ac, newText, oldText;
+    oldConsoleLog.apply(console, arguments);
+    ac = $("#aether-console");
+    oldText = ac.text();
+    newText = oldText + Array.prototype.slice.call(arguments).join(" ") + "\n";
+    return ac.text(newText).scrollTop(ac.prop('scrollHeight'));
+  };
+
+  loadConfig = function() {
+    editors[1].setValue(aetherInput.options);
+  };
+
+  aetherInput = {
+    options: 'var aetherOptions = {\n  thisValue: {say: console.log},\n  problems: {jshint_W040: {level: "ignore"}}\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\naether.run();\naether.run();\naether.run();\ndemoShowOutput(aether);'
+  };
+
+}).call(this);
