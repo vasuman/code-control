b894b482ad3a1bc0500d5baf7ebe63f785b12ca1
deepak1556 <hop2deep@gmail.com>
2014-03-01 20:00:11 +0530

    ace editor integrated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 // Modules
-
-// Entities
-
-// System
+//var aether = require('aether'); Y the F its not on npm??????? 
 
 // DOM Elements
 var dom = {
-  hud: document.getElementById('hud')
+  hud: document.getElementById('hud'),
+  editor: document.getElementById('javascript-input')
 }
 
-module.exports = Game
+module.exports = Editor
 
-function Game() {
+function Editor() {
+ var Range = ace.require('ace/range').Range, editor;
 
+ editor = ace.edit(dom.editor);
+ editor.setTheme('ace/theme/xcode');
+ editor.getSession().setUseWorker(false);
+ editor.getSession().setMode('ace/mode/javascript');
+ //editor.getSession().getDocument().on('change', watchForCodeChanges);
+
+ return editor;
 }
 
 // Start
-new Game
+new Editor
