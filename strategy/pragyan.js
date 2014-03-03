0c54ca6eb490d088fb46b35869f5812346dcf537
Vasuman R <vasumanar@gmail.com>
2014-03-03 19:26:14 +0530

    Added a pragyan _PHPSESSID passport strategy

---
+++ strategy/pragyan.js
@@ -0,0 +1,35 @@
+var passport = require('passport'),
+util = require('util');
+
+function PragyanStrategy(options, verify) {
+    if(options instanceof Function) {
+        verify = options;
+        options = {};
+    }
+    passport.Strategy.call(this);
+    this.name = 'pragyan';
+    this._verify = verify;
+    this._key = options.key || '_PHPSESSID';
+}
+
+util.inherits(PragyanStrategy, passport.Strategy);
+
+PragyanStrategy.prototype.authenticate = function(req) {
+    var sessionID = req.cookies[this._key];
+    if(!sessionID) {
+        return this.pass();
+    }
+    var self = this;
+    function verify(err, user) {
+        if(err) {
+            return self.error(err);
+        }
+        if(user == false) {
+            return self.fail();
+        }
+        self.success(user);
+    }
+    this._verify(sessionID, verify);
+}
+
+module.exports.PragyanStrategy = PragyanStrategy;
